97cfa39c64ed67d245e0bf7cb699aea4965a69ae
ilovecrayons <huangfeiyang2008@gmail.com>
2024-07-19 15:04:38 -0700

    USACO mixing milk

---
+++ build/CMakeFiles/comp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comp.dir/main.cpp.obj"
+  "CMakeFiles/comp.dir/main.cpp.obj.d"
+  "comp.exe"
+  "comp.exe.manifest"
+  "comp.pdb"
+  "libcomp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/comp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
 #include <vector>
 int main(){
-    int n;
-    std::cin >> n;
-    std::vector<std::pair<int, int>> v = {{1,0},{2,0},{3,0}};
-    for(int i = 0; i < n; i++){
-        int a, b, c;
-        std::cin >> a >> b >> c;
-        std::swap(v[a-1], v[b-1]);
-        v[c-1].second++;
+    int max1, max2, max3, cur1, cur2, cur3;
+    std::cin>>max1>>cur1;
+    std::cin>>max2>>cur2;
+    std::cin>>max3>>cur3;
+    for(int i=0; i<33; i++){
+        if(cur1+cur2<=max2){
+            cur2+=cur1;
+            cur1=0;
+        }else{
+            cur1-=max2-cur2;
+            cur2=max2;
+        }
+        if(cur2+cur3<=max3){
+            cur3+=cur2;
+            cur2=0;
+        }else{
+            cur2-=max3-cur3;
+            cur3=max3;
+        }
+        if(cur3+cur1<=max1){
+            cur1+=cur3;
+            cur3=0;
+        }else{
+            cur3-=max1-cur1;
+            cur1=max1;
+        }
     }
-    std::cout << std::max(v.at(0).second, std::max(v.at(1).second, v.at(2).second));
+    if(cur1+cur2<=max2){
+            cur2+=cur1;
+            cur1=0;
+    }else{
+        cur1-=max2-cur2;
+        cur2=max2;
+    }
+    std::cout<<cur1<<std::endl<<cur2<<std::endl<<cur3;
 }
